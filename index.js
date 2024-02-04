3efe1b615606a62fddf716d8d9e606c9ba76418c
Huy <pham.huyson87@hotmail.com>
2024-02-04 17:15:26 -0500

    Add shell-code

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+    - Based on
+        - Code a Crypto Whale Tracker Step-by-Step with Ethers.js
+        - https://www.youtube.com/watch?v=u_lwNJobmAI&t=507s&ab_channel=DappUniversity
+        - https://github.com/dappuniversity/whale_tracker/blob/master/solution.js
+*/
+
+
+const { ethers, Contract } = require('ethers')
